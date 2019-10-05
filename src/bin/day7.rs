fa9bdc64b2a88315bc60c1af5a697891d3f3bcca
lesnyrumcajs <lesny.rumcajs@gmail.com>
2019-10-05 15:31:13 +0200

    found the correct weight of the program

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,34 +1,42 @@
+use advent_of_rust_2017::helpers::*;
+use itertools::Itertools;
+use petgraph::prelude::*;
+use petgraph::visit::*;
 use regex::Regex;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use petgraph::graphmap::GraphMap;
-use petgraph::prelude::*;
-use petgraph::graph::node_index;
-use petgraph::visit::*;
 
 #[macro_use]
 extern crate lazy_static;
 
 fn main() {
-    day1();
+    day1_2();
+}
+
+#[derive(Hash, PartialEq, PartialOrd, Eq, Ord, Debug)]
+struct Program {
+    pub name: String,
+    pub weight: i32,
 }
 
 struct Tower {
-    pub name: String,
-    pub weight: i32,
+    pub program: Program,
     pub disks_above: Option<Vec<String>>,
 }
 
 impl Tower {
     fn from_line(line: String) -> Tower {
         lazy_static! {
-        static ref RE: Regex =
-            Regex::new("^(?P<name>\\w+) \\((?P<weight>\\d+)\\)(?: -> (?P<next>.*))?$").unwrap();
+            static ref RE: Regex =
+                Regex::new("^(?P<name>\\w+) \\((?P<weight>\\d+)\\)(?: -> (?P<next>.*))?$").unwrap();
         }
         let captures = RE.captures(line.as_str()).unwrap();
         Tower {
-            name: captures.name("name").unwrap().as_str().to_owned(),
-            weight: captures.name("weight").unwrap().as_str().parse().unwrap(),
+            program: Program {
+                name: captures.name("name").unwrap().as_str().to_owned(),
+                weight: captures.name("weight").unwrap().as_str().parse().unwrap(),
+            },
             disks_above: match captures.name("next") {
                 Some(x) => Some(x.as_str().split(", ").map(|x| x.to_owned()).collect()),
                 None => None,
@@ -41,8 +49,8 @@ impl Tower {
 fn simple_disk() {
     let line = "pbga (66)".to_owned();
     let tower = Tower::from_line(line);
-    assert_eq!(tower.name, "pbga");
-    assert_eq!(tower.weight, 66);
+    assert_eq!(tower.program.name, "pbga");
+    assert_eq!(tower.program.weight, 66);
     assert_eq!(tower.disks_above, None);
 }
 
@@ -50,39 +58,86 @@ fn simple_disk() {
 fn complex_disk() {
     let line = "fwft (72) -> ktlj, cntj, xhth".to_owned();
     let tower = Tower::from_line(line);
-    assert_eq!(tower.name, "fwft");
-    assert_eq!(tower.weight, 72);
+    assert_eq!(tower.program.name, "fwft");
+    assert_eq!(tower.program.weight, 72);
     assert_eq!(tower.disks_above.unwrap(), vec!["ktlj", "cntj", "xhth"]);
 }
 
-fn day1() {
-    let reader = BufReader::new(File::open("res/day7_example.in").unwrap());
+fn day1_2() {
+    let reader = BufReader::new(File::open("res/day7.in").unwrap());
     let values: Vec<Tower> = reader
         .lines()
         .map(|l| Tower::from_line(l.unwrap()))
         .collect();
 
-    let mut graph = GraphMap::new();
+    let mut graph = DiGraphMap::new();
+
+    let mut programs = HashMap::new();
+    for tower in &values {
+        programs
+            .entry(&tower.program.name)
+            .or_insert(&tower.program);
+    }
+
+    for tower in &values {
+        graph.add_node(&tower.program);
+    }
 
     for tower in &values {
         if tower.disks_above.is_some() {
             for x in tower.disks_above.as_ref().unwrap() {
-                graph.add_edge(&tower.name, &x, tower.weight);
+                graph.add_edge(&tower.program, &programs[x], 0);
             }
-        } else {
-            graph.add_node(&tower.name);
         }
     }
 
-    let graph: Graph<&String, i32, Directed> = graph.into_graph();
-
-    let tower_bottom = graph.raw_nodes()
+    let graph: Graph<&Program, i32, Directed> = graph.into_graph();
+    let tower_bottom = graph
+        .raw_nodes()
         .iter()
         .find(|&node| node.next_edge(Incoming) == EdgeIndex::end())
         .expect("No node without incoming edges!")
         .weight;
 
-    println!("Tower bottom: {}", tower_bottom);
+    println!("Tower bottom: {}", tower_bottom.name);
 
+    let weight = calculate_correct_program_weight(&graph, &tower_bottom);
+    println!("Corrected program weight: {}", weight);
+}
 
+fn calculate_correct_program_weight(
+    graph: &Graph<&Program, i32, Directed, u32>,
+    tower_bottom: &Program,
+) -> i32 {
+    let mut node = graph
+        .node_indices()
+        .find(|&n| graph.node_weight(n).unwrap() == &tower_bottom)
+        .unwrap();
+
+    let mut diff = 0;
+    loop {
+        let sums = graph
+            .neighbors_directed(node, Outgoing)
+            .map(|n| {
+                (
+                    n,
+                    Bfs::new(&graph, n)
+                        .iter(&graph)
+                        .map(|n| graph.node_weight(n).unwrap().weight)
+                        .sum::<i32>(),
+                )
+            })
+            .collect_vec();
+        let weights = sums.iter().map(|n| n.1).collect_vec();
+        let different_element_index = container_helpers::first_different_element(&weights);
+        if different_element_index.is_none() {
+            return graph.node_weight(node).unwrap().weight - diff;
+        }
+        let index = different_element_index.unwrap();
+        if different_element_index.unwrap() != 0 {
+            diff = (weights[index] - weights[0]).abs();
+        }
+
+        node = sums[different_element_index.unwrap()].0;
+    }
 }
---
+++ src/helpers/container_helpers.rs
@@ -0,0 +1,49 @@
+pub fn first_different_element<T>(v: &[T]) -> Option<usize>
+where
+    T: Eq,
+{
+    if v.len() < 3 {
+        return None;
+    }
+
+    if v[0] == v[1] && v[0] != v[2] {
+        return Some(2);
+    }
+
+    if v[0] == v[2] && v[0] != v[1] {
+        return Some(1);
+    }
+
+    if v[1] == v[2] && v[0] != v[1] {
+        return Some(0);
+    }
+
+    for (i, el) in v.iter().enumerate().skip(3) {
+        if el != &v[i - 1] {
+            return Some(i);
+        }
+    }
+
+    None
+}
+
+#[test]
+fn different_elements() {
+    let v = vec![1, 1, 1, 3, 1];
+    assert_eq!(first_different_element(&v), Some(3));
+
+    let v: Vec<String> = Vec::new();
+    assert_eq!(first_different_element(&v), None);
+
+    let v: Vec<i32> = vec![3, 1, 1];
+    assert_eq!(first_different_element(&v), Some(0));
+
+    let v: Vec<i32> = vec![1, 3, 1];
+    assert_eq!(first_different_element(&v), Some(1));
+
+    let v: Vec<i32> = vec![1, 1, 3];
+    assert_eq!(first_different_element(&v), Some(2));
+
+    let v: Vec<i32> = vec![1, 1, 1];
+    assert_eq!(first_different_element(&v), None);
+}
